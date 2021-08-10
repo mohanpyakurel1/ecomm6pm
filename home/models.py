3a396fb081d402a8bdf896fd23995cca2f759330
mohanpyakurel1 <mohanpyakurel1@gmail.com>
2021-08-10 14:53:02 +0545

    models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title=models.CharField(max_length=400)
+    slug=models.CharField(max_length=500, unique=True)
+    description=models.TextField()
+    def __str__(self):
+        return self.title
+class SubCategory(models.Model):
+    title=models.CharField(max_length=400)
+    slug=models.CharField(max_length=500, unique=True)
+    description=models.TextField()
+    def __str__(self):
+        return self.title
+
+class Slider(models.Model):
+    title=models.CharField(max_length=400)
+    slug=models.CharField(max_length=500, unique=True)
+    description=models.TextField()
+    image=models.ImageField(upload_to='media')
+    rank=models.IntegrField()
+    status=models.CharField(choices=(('active','active'), ('', 'inactive')))
+    def __str__(self):
+        return self.title
